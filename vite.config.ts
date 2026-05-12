e3c29d4a397f6c3aee7e36de75d18cdeef7eb991
RaihanArdianata <raihan.ardianata@gmail.com>
2026-05-13 04:09:12 +0700

    update orign

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
+  preview: {
+    host: "0.0.0.0",
+    port: 3001,
+    allowedHosts: true,
+  },
   plugins: [
     devtools(),
     paraglideVitePlugin({
